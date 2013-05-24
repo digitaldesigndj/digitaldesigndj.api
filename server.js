b1ca29e29545117e79babcc15d0c5a491734b245
Taylor Young <taylor@x-team.com>
2013-05-23 21:04:17 -0500

    cleanout

--- server.js
+++ server.js
@@ -1,110 +1,89 @@
-var app = require('express')()
-	// Redirected with nginx to /api
-  , socketio = require('socket.io')
-	, clientio = socketio.listen(8880)
-	, server = require('http').createServer(app)
-	, io = socketio.listen(server)
-	, port = 9779;
+// var app = require('express')
+// 	// Redirected with nginx to /apiclea
+// 	, socketio = require('socket.io')
+// 	, clientio = socketio.listen(8880)
+// 	, server = require('http').createServer(app)
+// 	, io = socketio.listen(server)
+// 	, port = 9779;
 
-// Twitter Setup
-var twitter = require('ntwitter');
-
-var twit = new twitter({
-	consumer_key: 'R96RyfVvpZu9JpbBtfrJzg'
-	, consumer_secret: '0O2YWco9k6dCkpO6vj2zIokpsmjUfdfRphV0UZ6as'
-	, access_token_key: '13287232-xZ7pAsI21qDFxoS26xPCrEBtw9aANUWxW4EZFNuez'
-	, access_token_secret: '7qrf1B6voo53duPpgOfFZ5obdgPhmscWelCV4eKAsE'
-});
-
-var connectCounter = 0;
+// var connectCounter = 0;
 
 // configure /api here
-clientio.set('resource','/api/socket.io');
-console.log( '' )
+// clientio.set('resource','/api/socket.io');
+// console.log( '' );
 
-// admin interface
-server.listen( port );
-console.log( 'admin interface listening on port:' + port );
+// // admin interface
+// server.listen( port );
+// console.log( 'admin interface listening on port:' + port );
 
-// admin interface route
-app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
-});
+// // admin interface route
+// app.get('/', function (req, res) {
+// 	res.sendfile(__dirname + '/index.html');
+// });
 
 // Socket connection to the admin interface
-io.sockets.on('connection', function (socket) {
-	console.log( 'admin conneted' );
-	socket.on('senttext', function (data) {
-		console.log(data);
-		clientio.sockets.emit('news', { messageFromControl: data } );
-	});
-	clientio.sockets.emit('news', { messageFromControl: 'publishing server connected' } );
-});
+// io.sockets.on('connection', function (socket) {
+// 	console.log( 'admin conneted' );
+// 	socket.on('senttext', function (data) {
+// 		console.log(data);
+// 		clientio.sockets.emit('news', { messageFromControl: data } );
+// 	});
+// 	clientio.sockets.emit('news', { messageFromControl: 'publishing server connected' } );
+// });
 
 // Publish changes to the admin system on the public socket
 // of('/api') is an important gotcha
-clientio.of('/api').on('connection', function (socket) {
-	console.log( 'client connected' );
-	// connectCounter++; console.log( 'client connected ' + connectCounter );
-	socket.emit('news', { messageFromControl: 'connected' } );
-  pollLastFm();
-});
-
-// clientio.of('/api').on('disconnection', function() { connectCounter--; console.log( 'client disconnected ' + connectCounter ); });
-
-// twit.stream('statuses/sample', function(stream) {
-// 	stream.on('data', function (data) {
-// 		// console.log(data);
-// 		clientio.sockets.emit('news', { messageFromControl: data } );
-// 	});
+// clientio.of('/api').on('connection', function (socket) {
+// 	console.log( 'client connected' );
+// 	// connectCounter++; console.log( 'client connected ' + connectCounter );
+// 	socket.emit('news', { messageFromControl: 'connected' } );
+// 	pollLastFm();
 // });
 
-twit.stream('user', {track:'BTtoronto'}, function(stream) {
-  stream.on('data', function (data) {
-    console.log(data);
-    clientio.sockets.emit('news', { messageFromControl: data.text } );
-  });
-  stream.on('end', function (response) {
-    // Handle a disconnection
-    console.log( 'disconnected');
-  });
-  stream.on('destroy', function (response) {
-    // Handle a 'silent' disconnection from Twitter, no end/error event fired
-    console.log( 'silent disconnect');
-  });
-  // Disconnect stream after five seconds
-  // setTimeout(stream.destroy, 5000);
+var io = require('socket.io').listen(8880);
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('update', { hello: 'world' });
+	// socket.on('my other event', function (data) {
+	// 	console.log(data);
+	// });
 });
 
-var lastfm;
+var lastfm = {};
+var total = 0;
 var username       = "DigitalDesignDj";
 var lastfm_api_key = 'c7b66efb5c1869ed420b3275da989fab';
-var hb             = require('handlebars');
 var $              = require('jquery');
+// var hb             = require('handlebars');
+
 
 function pollLastFm(){
-  $.ajax({
-    url: 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + encodeURIComponent(username) + '&api_key=' + encodeURIComponent(lastfm_api_key) + '&format=json',
-    dataType: "json",
-    success: function (data) {
-      if (data) {
-        // console.log(data);
-        if( data !== lastfm ){
-          lastfm = data;
-          console.log(lastfm);
-          //clientio.sockets.emit('news', { messageFromControl: lastfm. } );
-          clientio.sockets.emit('news', { messageFromControl: lastfm } );
-        }
-      } else {
-        console.log(data);
-      }
-    }
-  });
+	$.ajax({
+		url: 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + encodeURIComponent(username) + '&api_key=' + encodeURIComponent(lastfm_api_key) + '&format=json',
+		dataType: "jsonp",
+		success: function (data) {
+			console.log(data.recenttracks['@attr'].total); //.@attr.total);
+			lastfm = data;
+		}
+	});
+	return lastfm
 }
 
 var cronJob    = require('cron').CronJob;
 
 new cronJob('* * * * * *', function(){
-  console.log('Polling LastFM');
-  pollLastFm();
+	console.log('Polling LastFM');
+	pollLastFm();
+	if( lastfm.recenttracks ){
+		console.log( lastfm );
+		var current = lastfm.recenttracks['@attr'].total;
+		if( total !== current ){
+			total = current;
+			socket.emit('update', { 'lastfm': lastfm } );
+		}
+	}
+//	var current = lastfm.recenttracks['@attr'].total;
+	console.log( current );
+	
+	// console.log( pollLastFm() ); // .recenttracks['@attr'].total );
 }, null, true);
